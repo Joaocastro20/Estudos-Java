aaaa73a0773d1e2a280dbb0d1f3880eba7431469
Joaocastro20 <98284593+Joaocastro20@users.noreply.github.com>
2022-03-13 13:40:34 -0300

    Update MediaMediana.java

--- MediaMediana.java
+++ MediaMediana.java
@@ -7,23 +7,31 @@ public class NewClass {
         while (loop == true){
             //Instanciei o objeto scanner em "inserir"
             Scanner inserir = new Scanner(System.in);
+
             //Usei dois souts para informar o usuario a entrada dos dados
             System.out.println("Digite o valor de A:");
             int a = inserir.nextInt();
             System.out.println("Digite o valor de B:");
             int b = inserir.nextInt();
-        
+
             //uma das restrições imposta no exercicio é B menor que A
             if (b < a){
-                System.out.println("o valor A nao pode ser maior que o valor B!");
-            }else if (b > 1000000000){
-                //outra restrição é B nao pode ultrapassar o valor 10 elevado a 9
+                System.out.println("o valor A não pode ser maior que o valor B!");
+            }
+            //outra restrição é B nao pode ultrapassar o valor 10 elevado a 9
+            else if (b > 1000000000){
                 System.out.println("O valor B não pode ultrapassar 1.000.000.000");
-            }else{
-                //em uma relação onde A+B+C/3 = A logo vemos que C = (3*A)-(A+B)
+            }
+            //não menos importante o valor A nao pode ser menor que 1
+            else if(a < 0){
+            System.out.println("O valor A não pode ser menor que 1!");
+            }
+            //em uma relação onde A+B+C/3 = A, logo vemos que C = (3*A)-(A+B)
+            else{
 	            int c = ((3*a)-(a+b));
                 //A formula realiza a conta e exibe o valor de C
-                System.out.println("o valor minimo para media e mediana ser a mesma é "+ c);
+                System.out.println("o valor minimo de C para média e mediana ser a mesma é "+ c);
+                //caso os valores digitados pelo usuario satisfaça as restriçoes o loop termina
                 loop = false;
             }
         }
