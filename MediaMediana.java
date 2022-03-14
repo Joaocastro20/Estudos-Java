89e4669b7ba89b031b05220eebcc8f688607fc8b
Joaocastro20 <98284593+Joaocastro20@users.noreply.github.com>
2022-03-14 20:11:30 -0300

    Update MediaMediana.java

--- MediaMediana.java
+++ MediaMediana.java
@@ -1,9 +1,10 @@
-//Comecei importando uma a classe Scanner do pacote java.util
+//Comecei importando a classe Scanner do pacote java.util
 import java.util.Scanner;
-//Criei uma classe para realizar o codigo
+//Criei uma classe para realizar o código
 public class NewClass {
     public static void main(String[] args) {
         boolean loop = true;
+        //optei por deixar so um loop no codigo
         while (loop == true){
             //Instanciei o objeto scanner em "inserir"
             Scanner inserir = new Scanner(System.in);
@@ -13,7 +14,7 @@ public class NewClass {
             int a = inserir.nextInt();
             System.out.println("Digite o valor de B:");
             int b = inserir.nextInt();
-
+            //usei um if para cada situação para ficar mais intuitivo para o usuario
             //uma das restrições imposta no exercicio é B menor que A
             if (b < a){
                 System.out.println("o valor A não pode ser maior que o valor B!");
@@ -26,11 +27,11 @@ public class NewClass {
             else if(a <= 0){
             System.out.println("O valor A não pode ser menor que 1!");
             }
-            //em uma relação onde A+B+C/3 = A, logo vemos que C = (3*A)-(A+B)
             else{
+                //em uma relação onde A+B+C/3 = A, logo vemos que C = (3*A)-(A+B)
 	            int c = ((3*a)-(a+b));
                 //A formula realiza a conta e exibe o valor de C
-                System.out.println("o valor minimo de C para média e mediana ser a mesma é "+ c);
+                System.out.println("o valor minimo de C para média e mediana serem a mesma é "+ c);
                 //caso os valores digitados pelo usuario satisfaça as restrições o loop termina
                 loop = false;
             }
