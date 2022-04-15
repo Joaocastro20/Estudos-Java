52b0947fd6c6ef318d4d02c5ec396252a6057095
Joaocastro20 <joaopedroabreucastro@hotmail.com>
2022-04-15 19:35:50 -0300

    java 1

--- java-1/src/com/company/Main.java
+++ java-1/src/com/company/Main.java
@@ -1,8 +1,18 @@
 package com.company;
 
 public class Main {
-
+    //função main é a principl que torna a classe em um programa
     public static void main(String[] args) {
-	    System.out.println("ola, mundo");
+	    //declaração de variaveis
+        //variavel sempre em letra minuscula começando != numeros
+        int n1;
+        int n2,n3,n4;
+        int n5 = 1, n6 = 2;
+        String c = "joao";
+        float m = 3.5F;
+        System.out.println(n5);
+        System.out.println(c);
+        //compilador bytecode
+        System.out.println("meu nome é " +c);
     }
 }
