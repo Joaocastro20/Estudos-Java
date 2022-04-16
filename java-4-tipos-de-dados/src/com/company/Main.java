e4ab65568b0d2702d6d13e4e09b7d6539e01ba7d
Joaocastro20 <joaopedroabreucastro@hotmail.com>
2022-04-16 11:21:09 -0300

    java-4

---
+++ java-4-tipos-de-dados/src/com/company/Main.java
@@ -0,0 +1,28 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+	//numeros primarios
+        int n1 = 999999999;
+        short n2 = 9999;
+        float n5 = 23.4f;
+        double n6 = 2.3;
+        byte n9 =  100;
+        char l = (char) (n9 + 97);
+        String letras = "amsdkawkdakwmdakwmd";
+        
+    //nao primarios
+        Integer n3 = 999999999;
+        Short n4 = 10000;
+        Float n7 = 23.52312231231231232312313132f;
+        Double n8 = 2.412312312312313212313212312;
+        Byte n10 = 100;
+        System.out.println(Byte.SIZE);
+        System.out.println(Byte.MAX_VALUE);
+        System.out.println(Byte.MIN_VALUE);
+        System.out.println(l);
+        System.out.println(Character.SIZE * letras.length());
+    }
+}
