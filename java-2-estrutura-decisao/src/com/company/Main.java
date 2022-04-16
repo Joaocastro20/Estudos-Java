fa5edd0c8368faf93637dfdadbbd2a335bcc89e5
Joaocastro20 <98284593+Joaocastro20@users.noreply.github.com>
2022-04-15 21:09:08 -0300

    java 2

--- java-2-estrutura-decisao/src/com/company/Main.java
+++ java-2-estrutura-decisao/src/com/company/Main.java
@@ -1,8 +1,24 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+    //estrutura de decisao if, else
+	Scanner leitor = new Scanner(System.in);
+    System.out.println("Digite um numero e faça verificaçoes:");
+    int n = leitor.nextInt();
+    if (n > 0){
+        System.out.println("o numero "+n+" é positivo");
+    }else{
+        System.out.println("o numero "+n+" é negativo");
+    }
+    if(n % 2 == 0){
+        System.out.println("o numero "+n+" é par");
+    }else{
+        System.out.println("o numero "+n+" é impar");
+    }
+
     }
 }
