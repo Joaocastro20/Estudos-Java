19e41a40b50005d8227a653c4ac8e7ea702ec564
JoaoCastro20 <@Charmileon1>
2025-05-18 16:44:18 -0300

    adicionar modulo basico uso de prints

---
+++ OracleJava21zo830/BasicOfJava/BasicOfJava.java
@@ -0,0 +1,7 @@
+package OracleJava21zo830.BasicOfJava;
+
+public class BasicOfJava {
+    public static void main(String[] args) {
+        System.out.println("Basic");
+    }
+}
---
+++ OracleJava21zo830/BasicOfJava/PrintsTypes.java
@@ -0,0 +1,17 @@
+package OracleJava21zo830.BasicOfJava;
+
+public class PrintsTypes {
+    public static void main(String[] args) {
+        System.out.println("Proxima linha");
+        System.out.println("Proxima" + " linha");
+        System.out.print("Proxima");
+        System.out.print(" linha");
+        int a = 1;
+        int b = 2;
+        int c = a + b;
+        System.out.printf("%d + %d = %d",a,b,c);
+        System.out.printf("%S%n","hello");
+//        System.out.println();
+        System.out.printf(" WORD %n");
+    }
+}
