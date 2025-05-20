3b91ef3b3cc5ca95468c68e1e19f50bda5279b2d
JoaoCastro20 <@Charmileon1>
2025-05-20 12:42:10 -0300

    adicionar variaveis

---
+++ OracleJava21zo830/BasicOfJava/Variables.java
@@ -0,0 +1,25 @@
+package OracleJava21zo830.BasicOfJava;
+
+public class Variables {
+    public static void main(String[] args) {
+        int a = 10, b = 20, c = a + b;
+        System.out.printf("%d + %d = %d%n", a, b, c);
+        System.out.println("Result -> " + (a + b));
+
+        //Nomenclatura
+        int age; //valid
+        int _age; //valid
+        int $age; //valid
+        int age3; //valid
+
+        /*
+        int a ge; //invalid
+        int 3age; //invalid
+        int break; //invalid
+        int do; //invalid
+         */
+
+        //usar nomes mais completos em variaveis
+        int month;
+    }
+}
