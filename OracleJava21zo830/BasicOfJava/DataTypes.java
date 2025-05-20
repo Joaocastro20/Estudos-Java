cdab7450d997346047878a3db5aed41daabe27f9
JoaoCastro20 <sememail@gmail.com>
2025-05-20 15:43:57 -0300

    adicionar tipos de dados parcial

---
+++ OracleJava21zo830/BasicOfJava/DataTypes.java
@@ -0,0 +1,26 @@
+package OracleJava21zo830.BasicOfJava;
+
+public class DataTypes {
+    public static void main(String[] args) {
+        //PRIMITIVOS
+        boolean b;
+        char c;
+        int imin = -2147483648, imax = 2147483647, iValorPadrao = 0;
+        byte bmin = -128, bmax = 127, bValorPadrao = 0;
+        short smin = -32768, smax = 32767, sValorPadrao = 0;
+//        long lmin = −9223l ;
+        float f;
+        double d;
+        //NAO PRIMITIVOS
+        /*
+        Class
+        Enum
+        Interface
+        String
+        Array
+         */
+
+        //Byte
+
+    }
+}
