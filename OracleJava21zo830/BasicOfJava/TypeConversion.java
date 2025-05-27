c642cd2faafa730bf76d4c6b48a146617426a331
JoaoCastro20 <sememail@gmail.com>
2025-05-27 09:10:01 -0300

    adicionar tipo conversao

---
+++ OracleJava21zo830/BasicOfJava/TypeConversion.java
@@ -0,0 +1,16 @@
+package OracleJava21zo830.BasicOfJava;
+
+public class TypeConversion {
+    public static void main(String[] args) {
+        double d;
+        int b = 20;
+        d = b;
+        System.out.println(d);
+
+        double i = 135.55;
+        long c = (long) i;
+        System.out.println(c);
+
+
+    }
+}
