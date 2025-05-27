cb995370068c3966e353db8a1d44dcb1716fddb5
JoaoCastro20 <sememail@gmail.com>
2025-05-27 20:24:52 -0300

    adicionar operadores equalitarios e relacionais

---
+++ OracleJava21zo830/Operators/EqualityRelationalOperators.java
@@ -0,0 +1,18 @@
+package OracleJava21zo830.Operators;
+
+public class EqualityRelationalOperators {
+    public static void main(String[] args) {
+        int um = 1;
+        int dois = 2;
+        int tres = 3;
+        int quatro = 4;
+        int seis = 6;
+        int sete = 7;
+        if(um == 1) System.out.println("1");
+        if(dois != 1) System.out.println("2");
+        if(sete > seis) System.out.println("é maior");
+        if(um < dois ) System.out.println("é menor");
+        if(tres >= 3) System.out.println("E igual ou maior");
+        if(3 <= tres) System.out.println("é maior igual");
+    }
+}
