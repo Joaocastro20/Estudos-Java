77c0bdbf9e996ad44ae44b813a38cc599bfdb186
JoaoCastro20 <sememail@gmail.com>
2025-05-27 20:09:06 -0300

    adicionar operadores unitarios

---
+++ OracleJava21zo830/Operators/UnaryOperators.java
@@ -0,0 +1,19 @@
+package OracleJava21zo830.Operators;
+
+public class UnaryOperators {
+    public static void main(String[] args) {
+        int a = 10;
+        boolean b = true;
+        System.out.println("Valor Inicial 10");
+        System.out.println("+ ---> " + a);
+        System.out.println("- ---> " + -a);
+        System.out.println("++ ---> " + ++a);
+        System.out.println("-- ---> " + --a);
+        System.out.println("! ---> " + !b);
+
+        /*
+        quando utilizamos o prefixo ele valida e aumenta o valor na execuxao
+        no sufixo ele almenta apos a execuçao
+         */
+    }
+}
