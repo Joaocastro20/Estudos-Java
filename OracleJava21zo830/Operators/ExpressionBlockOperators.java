013e4b9980ba2155a0ca7466c49d9d2fe260f0c2
JoaoCastro20 <sememail@gmail.com>
2025-05-30 09:29:08 -0300

    adicionar operadores exercices

---
+++ OracleJava21zo830/Operators/ExpressionBlockOperators.java
@@ -0,0 +1,14 @@
+package OracleJava21zo830.Operators;
+
+public class ExpressionBlockOperators {
+    public static void main(String[] args) {
+        int x = 2 + 5 / 2 % 7 * 4 - 2;
+        System.out.println(x);
+
+
+        System.out.print(11 / 2);
+        System.out.print(11 / 2.0);
+        System.out.print(11 % 2);
+        System.out.print(11 % 2.0);
+    }
+}
