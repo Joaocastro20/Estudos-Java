f1a577c2e4b836b5af9cd1c3f277697aa0619951
JoaoCastro20 <sememail@gmail.com>
2025-06-04 08:30:19 -0300

    adicionar controlFlow ternary

---
+++ OracleJava21zo830/ControlFlow/Ternary.java
@@ -0,0 +1,9 @@
+package OracleJava21zo830.ControlFlow;
+
+public class Ternary {
+    public static void main(String[] args) {
+        boolean control = true;
+        String msg = control ? "Sim" : "Nao";
+        System.out.println(msg);
+    }
+}
