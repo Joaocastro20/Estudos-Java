63577b0eec9ae24a4b7a6438420ad9960a879cc8
JoaoCastro20 <sememail@gmail.com>
2025-06-04 18:16:29 -0300

    adicionar controlFlow infinite loop

---
+++ OracleJava21zo830/ControlFlow/InfiniteLoop.java
@@ -0,0 +1,11 @@
+package OracleJava21zo830.ControlFlow;
+
+public class InfiniteLoop {
+    public static void main(String[] args) {
+//        final boolean control = false;
+//        for (control) {
+//            System.out.println("Loop infinito com for.");
+//        }
+//        int a = 0;
+    }
+}
