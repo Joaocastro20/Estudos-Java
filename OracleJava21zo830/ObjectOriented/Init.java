9a08bdbd4d05c76bd50f205e0e1fd0c6f35544ee
JoaoCastro20 <sememail@gmail.com>
2025-06-07 18:26:47 -0300

    adicionar method init

---
+++ OracleJava21zo830/ObjectOriented/Init.java
@@ -0,0 +1,10 @@
+package OracleJava21zo830.ObjectOriented;
+
+public class Init {
+    public static void main(String[] args) {
+        Modifieds modifieds = new Modifieds();
+        modifieds.publico();
+        modifieds.protegido();
+        modifieds.padrao();
+    }
+}
