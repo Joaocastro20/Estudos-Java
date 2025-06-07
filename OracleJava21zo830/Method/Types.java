8ac3c271b5e3c3c2070c577c0e49c4a73740adb5
JoaoCastro20 <sememail@gmail.com>
2025-06-07 14:10:36 -0300

    adicionar method var args

--- OracleJava21zo830/Method/Types.java
+++ OracleJava21zo830/Method/Types.java
@@ -1,7 +1,16 @@
 package OracleJava21zo830.Method;
 
+import java.util.ArrayList;
+
 public class Types {
     public static void main(String[] args) {
+        String[] nomes = {"jao", "maria", "jose"};
+        show(nomes);
+    }
 
+    static void show(String... lista){
+        for(String a : lista){
+            System.out.println(a);
+        }
     }
 }
